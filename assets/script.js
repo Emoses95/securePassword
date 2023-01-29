434eeb6e2fd913ae5681c7d836057de96db5f4eb
Emoses95 <moses.e.collins@gmail.com>
2023-01-29 13:25:05 -0800

    Added get randoms

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,23 @@
 // Assignment Code
 var generateBtn =document.querySelector("#generate");
 
+// this function needs more arguments such as: booleans which tell the function whether or not to include numbers/letters/etc in the generated password
 function generatePassword(){
   var length = window.prompt("choose password length between 8 and 128");
- char = "abcdefghijklmnopqrtuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()";
- num = "1234567890"
-  passwordText = [""];
+  var passwordChars = "abcdefghijklmnopqrtuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()1234567890";
+  var generatedPassword = "";
+  // somewhere in here there needs to be an if statement which uses the boolean arguments
   for(i=0; i< length; i++){
-  password += char.num(Math.floor(Math.random()* char.length))
-    return "passwordText" ;
+    // get random number
+    var randomIndex= Math.floor(Math.random()*passwordChars.length)
+    // chooose random character from lettersAndSymbols
+   var randomChar= passwordChars[randomIndex]
+    // Add random character
+  generatedPassword += randomChar
 }
-  
+return generatedPassword;
 }
-
+generatePassword
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
